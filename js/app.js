9695f1d7189b90045c8eae16151885c395d7914f
M404ttis <mattisluecke@gmail.com>
2023-02-09 13:20:50 +0100

    adds navID while dynamically building nav

--- js/app.js
+++ js/app.js
@@ -31,6 +31,32 @@ const sections = document.querySelectorAll('section');
  * 
 */
 
+const addNavEventListener = (navItem) => {
+    navItem.addEventListener('click', (event) => {
+        event.preventDefault();
+        const targetSection = document.querySelector(`[data-nav="${navItem.innerText}"]`);
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+    });
+}
+
+// Add class 'active' to section when near top of viewport 
+function makeActive(){
+    for (const section of sections) {
+        const box = section.getBoundingClientRect();
+        //Find a value that works best, but 150 seems to be a good start.
+        if (box.top <= 120 && box.bottom >= 121) {
+            //apply active state on current section and corresponding Nav link
+            section.classList.add('your-active-class');
+            // console.log(section);
+            navBar.querySelector(`${section.dataset.nav}`).classList.add('your-active-class');
+            console.log(navBar.getElementsByClassName(`${section.dataset.nav}`));
+            // .classList.add('your-active-class');
+        } else {
+            //Remove active state from other section and corresponding Nav link
+            section.classList.remove('your-active-class');
+        }
+    }
+}
 
 
 /**
@@ -40,23 +66,22 @@ const sections = document.querySelectorAll('section');
 */
 
 // build the nav
-
 const buildNavBar = () => {
     const navBarFragement = document.createDocumentFragment();
 
     for (const section of sections) {
         const navItem = document.createElement('li');
-        navItem.innerHTML = `<a class="menu__link" href="#${section.id}">${section.dataset.nav}</a>`;
+        navItem.innerHTML = `<a class="menu__link" id="nav_${section.id}">${section.dataset.nav}</a>`;
+        addNavEventListener(navItem);
         navBarFragement.appendChild(navItem);
     }
     navBar.appendChild(navBarFragement);
 }
 
 
-// Add class 'active' to section when near top of viewport
 
 
-// Scroll to anchor ID using scrollTO event
+
 
 
 /**
@@ -71,5 +96,6 @@ document.addEventListener('DOMCOntentLoaded', buildNavBar());
 // Scroll to section on link click
 
 // Set sections as active
+document.addEventListener("scroll", function() { makeActive();});
 
 
